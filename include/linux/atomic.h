9db3072a31a9f0fda7769d3994d79e44a5358c1d
diudiuyc5200 <17766728994@163.com>
2026-01-02 15:57:48 +0800

    Update atomic.h

--- include/linux/atomic.h
+++ include/linux/atomic.h
@@ -655,6 +655,7 @@ static inline int atomic_dec_if_positive(atomic_t *v)
 #endif
 
 #define atomic_cond_read_acquire(v, c)	smp_cond_load_acquire(&(v)->counter, (c))
+#define atomic_cond_read_relaxed(v, c)	smp_cond_load_relaxed(&(v)->counter, (c))
 
 #ifdef CONFIG_GENERIC_ATOMIC64
 #include <asm-generic/atomic64.h>
@@ -1076,6 +1077,7 @@ static inline long long atomic64_fetch_andnot_release(long long i, atomic64_t *v
 #endif
 
 #define atomic64_cond_read_acquire(v, c)	smp_cond_load_acquire(&(v)->counter, (c))
+#define atomic64_cond_read_relaxed(v, c)	smp_cond_load_relaxed(&(v)->counter, (c))
 
 #include <asm-generic/atomic-long.h>
 
