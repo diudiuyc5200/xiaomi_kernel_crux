6e911413e5a9047fa362e6d09584badd1566caed
diudiuyc5200 <17766728994@163.com>
2026-03-09 18:50:59 +0800

    Update alauda.c

--- drivers/usb/storage/alauda.c
+++ drivers/usb/storage/alauda.c
@@ -490,10 +490,12 @@ static int alauda_check_media(struct us_data *us)
 	}
 
 	/* Check for media change */
-	if (status[0] & 0x08) {
+	if (status[0] & 0x08 || !info->media_initialized) {
 		usb_stor_dbg(us, "Media change detected\n");
 		alauda_free_maps(&MEDIA_INFO(us));
-		alauda_init_media(us);
+		rc = alauda_init_media(us);
+		if (rc == USB_STOR_TRANSPORT_GOOD)
+			info->media_initialized = true;
 
 		info->sense_key = UNIT_ATTENTION;
 		info->sense_asc = 0x28;
