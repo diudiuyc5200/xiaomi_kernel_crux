2da59a542dcb0fa7766282e25f6d61e0ff85fcaa
diudiuyc5200 <17766728994@163.com>
2026-03-09 17:11:14 +0800

    Update core.c

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -394,7 +394,14 @@ static void dwc3_free_event_buffers(struct dwc3 *dwc)
 static int dwc3_alloc_event_buffers(struct dwc3 *dwc, unsigned length)
 {
 	struct dwc3_event_buffer *evt;
+unsigned int hw_mode;
 
+	hw_mode = DWC3_GHWPARAMS0_MODE(dwc->hwparams.hwparams0);
+	if (hw_mode == DWC3_GHWPARAMS0_MODE_HOST) {
+		dwc->ev_buf = NULL;
+		return 0;
+	}
+	
 	evt = dwc3_alloc_one_event_buffer(dwc, length);
 	if (IS_ERR(evt)) {
 		dev_err(dwc->dev, "can't allocate event buffer\n");
