2bb1f0f6d02a623be37c4da72cb75acd0c75638e
diudiuyc5200 <17766728994@163.com>
2026-03-10 15:54:49 +0800

    Update zram_drv.c

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1648,7 +1648,7 @@ out:
  */
 static blk_qc_t zram_make_request(struct request_queue *queue, struct bio *bio)
 {
-	struct zram *zram = queue->queuedata;
+	struct zram *zram = bio->bi_disk->private_data;
 
 	if (!valid_io_request(zram, bio->bi_iter.bi_sector,
 					bio->bi_iter.bi_size)) {
