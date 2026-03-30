df60ba176c543321c0c0be6c919252a2d2e8e86c
diudiuyc5200 <17766728994@163.com>
2026-03-30 17:28:41 +0800

    Update vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -129,7 +129,7 @@ struct scan_control {
 /*
  * Number of active kswapd threads
  */
-#define DEF_KSWAPD_THREADS_PER_NODE 16
+#define DEF_KSWAPD_THREADS_PER_NODE 20
 int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
 int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 
