83ce098c7e02a77f4d7396fd3a6eedf64f5f2a39
diudiuyc5200 <17766728994@163.com>
2026-07-03 11:02:44 +0800

    Add files via upload

--- drivers/power/supply/qcom/bq27z561_fg.c
+++ drivers/power/supply/qcom/bq27z561_fg.c
@@ -1186,7 +1186,7 @@ static int fg_set_property(struct power_supply *psy,
 		break;
 	/* 新增 charge_full 写入分支 */
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
-		if (val->intval >= 2000000 && val->intval <= 7000000) {
+		if (val->intval >= 2000000 && val->intval <= 20000000) {
 			bq->custom_charge_full = val->intval;
 			bq->charge_full_override = true;
 			power_supply_changed(bq->fg_psy);
